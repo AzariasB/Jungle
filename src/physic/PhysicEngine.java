18f140d0c89956604130cacd0eae26b62a24b623
Daniel Braikeh <danbraik@hotmail.com>
2015-02-06 23:07:53 +0100

    Add Game, PhysicEngine. rename Main

---
+++ src/physic/PhysicEngine.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physic;
+
+import org.jsfml.system.Time;
+
+/**
+ *
+ */
+public class PhysicEngine {
+
+    public void movePlayer(Direction direction) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void update(Time time) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public enum Direction {
+        UP,
+        LEFT,
+        DOWN,
+        RIGHT
+    }
+}
